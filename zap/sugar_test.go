0d12187dfa1bd5bc7b599ad2721c1391b421e02e
dapeng <wangju@openviewtech.com>
2025-03-22 11:17:14 +0800

    refactor(deps): update dependencies and refactor tracer integration
    - Update `github.com/gone-io/gone/v2` to v2.0.7- Update `github.com/sagikazarmark/locafero` to v0.8.0
    - Update `github.com/spf13/afero` to v1.14.0
    - Update `golang.org/x/text` to v0.23.0
    - Update `go.uber.org/multierr` to v1.11.0
    - Remove `github.com/gone-io/goner/tracer` dependency
    - Replace with `github.com/gone-io/goner/g` v1.0.0- Refactor tracer integration in `zap` package

--- zap/sugar_test.go
+++ zap/sugar_test.go
@@ -2,17 +2,36 @@ package gone_zap
 
 import (
 	"github.com/gone-io/gone/v2"
-	"github.com/gone-io/goner/tracer"
+	"github.com/gone-io/goner/g"
 	"os"
 	"testing"
 )
 
-func TestNewSugar(t *testing.T) {
+type tracer struct {
+	gone.Flag
+	g.Tracer
+}
 
+func (t *tracer) SetTraceId(traceId string, fn func()) {
+	fn()
+}
+
+// GetTraceId Get the traceId of the current goroutine
+func (t *tracer) GetTraceId() string {
+	return "trace-id"
+}
+
+// Go Start a new goroutine instead of `go func`, which can pass the traceId to the new goroutine.
+func (t *tracer) Go(fn func()) {
+	go fn()
+}
+
+func TestNewSugar(t *testing.T) {
 	os.Setenv("GONE_LOG_LEVEL", "debug")
 	gone.
-		NewApp(Priest, tracer.Load).
-		Test(func(log gone.Logger, tracer tracer.Tracer, in struct {
+		NewApp(Load).
+		Load(&tracer{}).
+		Test(func(log gone.Logger, tracer g.Tracer, in struct {
 			level string `gone:"config,log.level"`
 		}) {
 			log.Infof("level:%s", in.level)
